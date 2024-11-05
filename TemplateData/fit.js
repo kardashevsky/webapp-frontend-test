697f44a36316d2045666eb3c62122433281439f0
d.kardashevskiy <d.kardashevskiy@neuragames.online>
2024-11-05 17:42:53 +0300

    unity

---
+++ TemplateData/fit.js
@@ -0,0 +1,20 @@
+function fit(container, minRatioW, minRatioH, maxRatioW, maxRatioH) {
+  let currentR = window.innerWidth / window.innerHeight;
+
+  let minR = minRatioW / minRatioH;
+  let maxR = maxRatioW / maxRatioH;
+
+  let clampedR = Math.min(Math.max(currentR, minR), maxR);
+
+  let width = window.innerWidth;
+  let height = width / clampedR;
+
+  if (height > window.innerHeight) {
+    width = Math.min(width, Math.ceil(window.innerHeight * clampedR));
+  }
+
+  height = Math.floor(width / clampedR);
+  
+  container.style.width = width + "px";
+  container.style.height = height + "px";
+}
